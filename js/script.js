31edecae3715b9da3ebd17e1e99715769456986b
Tiến Anh Nguyễn <anhnt2111.work@gmail.com>
2021-12-06 08:56:42 +0700

    up

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        200: {
+        items: 1,
+        },
+    },
+    });
+
+});
